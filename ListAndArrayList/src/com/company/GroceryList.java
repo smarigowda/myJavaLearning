5f9ef2013c9b3823e3862330f679830297f3903d
Marigowda, Santosh <santosharakere@gmail.com>
2016-11-13 07:08:31 +0000

    refactoring to move the complexity out of the main class

--- ListAndArrayList/src/com/company/GroceryList.java
+++ ListAndArrayList/src/com/company/GroceryList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class GroceryList {
 
-    private ArrayList<String> groceryList = new ArrayList<String>();
+    private ArrayList<String> groceryList = new ArrayList<>();
 
     public void addGroceryItem(String item) {
         groceryList.add(item);
@@ -17,23 +17,39 @@ public class GroceryList {
         }
     }
 
-    public void modifyGroceryItem(int position, String newItem) {
+    public void modifyGroceryItem(String currentItem, String newItem) {
+        int position = findItem(currentItem);
+        if(position >= 0) {
+            modifyGroceryItem(position, newItem);
+        }
+    }
+
+    private void modifyGroceryItem(int position, String newItem) {
         groceryList.set(position, newItem);
         System.out.println("Item " + (position + 1) + " has been modified");
     }
 
-    public void removeGroceryItem(int position) {
-        String item = groceryList.get(position);
+    public void removeGroceryItem(String item) {
+        int position = findItem(item);
+        if( position >= 0 ) {
+            removeGroceryItem(position);
+        }
+    }
+    private void removeGroceryItem(int position) {
         groceryList.remove(position);
     }
 
-    public String findItem(String searchItem) {
-//        boolean exists = groceryList.contains(searchItem);
-        int position = groceryList.indexOf(searchItem);
-        if(position >= 0) {
-            return "item exists at position " + position;
+    private int findItem(String searchItem) {
+        return groceryList.indexOf(searchItem);
+
+    }
+
+    public boolean onFile(String item) {
+        int position = findItem(item);
+        if(position >= 0 ) {
+            return true;
         } else {
-            return null;
+            return false;
         }
     }
 }
--- ListAndArrayList/src/com/company/Main.java
+++ ListAndArrayList/src/com/company/Main.java
@@ -5,40 +5,82 @@ import java.util.Scanner;
 public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
-    private static int[] baseData = new int[10];
-
-
+    private static GroceryList glist = new GroceryList();
 
     public static void main(String[] args) {
-	    getInput();
-        printArray(baseData);
-        resizeArray();
-        System.out.println("Array resized to have 2 more elements:");
-        baseData[10] = 10;
-        baseData[11] = 11;
-        printArray(baseData);
+        boolean quit = false;
+        int choice = 0;
+        printInstructions();
+
+        while(!quit) {
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+            switch(choice) {
+                case 0:
+                    printInstructions();
+                    break;
+                case 1:
+                    glist.printGroceryList();
+                    break;
+                case 2:
+                    addItem();
+                    break;
+                case 3:
+                    modifyItem();
+                    break;
+                case 4:
+                    removeItem();
+                    break;
+                case 5:
+                    searchItem();
+                    break;
+                case 6:
+                    quit = true;
+                    System.out.println("exiting...");
+                    break;
+            }
+        }
+
     }
 
-    private static void getInput() {
-        System.out.println("Enter " + baseData.length + " integers:");
-        for(int i=0; i<baseData.length; i++) {
-            baseData[i] = scanner.nextInt();
+    public static void printInstructions() {
+        System.out.println("Press ");
+        System.out.println("\t 0 - print options");
+        System.out.println("\t 1 - print list");
+        System.out.println("\t 2 - add an item");
+        System.out.println("\t 3 - modify an item");
+        System.out.println("\t 4 - remove an item");
+        System.out.println("\t 5 - search an item");
+        System.out.println("\t 6 - quit");
+    }
+
+    public static void addItem() {
+        System.out.print("enter a grocery item: ");
+        glist.addGroceryItem(scanner.nextLine());
+    }
+
+    public static void modifyItem() {
+        System.out.print("enter current item name: ");
+        String itemName = scanner.nextLine();
+        System.out.println("enter replacement item name: ");
+        String newItemName = scanner.nextLine();
+        glist.modifyGroceryItem(itemName, newItemName);
+    }
+
+    public static void removeItem() {
+        System.out.print("enter item name to remove: ");
+        String itemName = scanner.nextLine();
+        glist.removeGroceryItem(itemName);
+    }
+
+    public static void searchItem() {
+        System.out.println("Enter the item name to search for: ");
+        String searchItem = scanner.nextLine();
+        if(glist.onFile(searchItem)) {
+            System.out.println("item found in the list");
+        } else {
+            System.out.println("item does not exist in our list");
         }
     }
-
-    private static void printArray(int[] array) {
-        for(int i=0; i<baseData.length; i++) {
-            System.out.print(baseData[i] + " ");
-        }
-        System.out.println();
-    }
-
-    private static void resizeArray() {
-        int[] origiinal = baseData;
-        baseData = new int[12];
-        for(int i=0; i<origiinal.length; i++) {
-            baseData[i] = origiinal[i];
-        }
-    }
-
 }
